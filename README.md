9a441466d5017d489d1486ba521085eda11bf30f
MikaelEpelbaum <mikaelepelbaum@gmail.com>
2024-01-23 18:31:11 +0200

    docu added

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-"# au10tix" 
+This repository is a homework assignment from au10tix made by me Mikael Epelbaum.
+
+To run the code you can install the requirements
+
+run the following command in the directory.
+pip install -r requirements.txt 
+
+now you can run the code from the main.py
+
+it will print two rows.
+the first is the number of open issues in the given directory
+the second is the number of open issues with the label "practice1"
+
+
+I have used the requests and the json libraries in this repo.
+I have created a configuration file where you can find the parameters for the given repository
+you can change it to a new repository if desired.
--- main.py
+++ main.py
@@ -10,18 +10,21 @@ class Github():
             'Authorization': "Bearer " + self.conf['token'],
             'X-GitHub-Api-Version': '2022-11-28'
         }
-        self.endpoint = 'https://api.github.com/repos/{}/{}/issues'.format(self.conf['rep_owner'], self.conf['rep_name'])
+        self.endpoint = self.conf['github_api'].format(self.conf['rep_owner'], self.conf['rep_name'])
 
+    # returns a list of all the open issues in the repo
     def open_issues(self):
         response = requests.get(self.endpoint, headers=self.headers).json()
         open_issues = [issue for issue in response if issue['state'] == 'open']
         return open_issues
 
+    # prints the issues from the repo which have the desired label
     def num_of_labeled_issues(self, targ_label: str):
         response = self.open_issues()
         labled_issues = [issue for issue in response if any(label.get('name') == targ_label for label in issue.get('labels', []))]
         print(len(labled_issues))
 
+    # creates and validates the creation of an issue returns the creation response
     def create_issue(self, name: str, lang: str):
         body = {"title": f"{name}'s  issue",
                 "body": f"This issue was created via REST API from {lang} by {name}",
@@ -31,31 +34,23 @@ class Github():
         assert response.status_code == 201
         return response.json()
 
-    def update_issue_state(self, number: int):
-        endpoint = self.endpoint + f'/{number}'
-        body = {'state': 'closed', 'state_reason': 'not_planned'}
-        response = requests.patch(endpoint,  json.dumps(body), headers=self.headers)
-        assert response.status_code == 200
-
+    # validates issue has been correctly added
     def test_issues_increament(self, initial_number_of_issues: int, new_issue: json):
         new_list_of_issues = git.open_issues()
         assert initial_number_of_issues + 1 == len(new_list_of_issues)
         assert json.dumps(new_issue, sort_keys=True), json.dumps(new_list_of_issues[0], sort_keys=True)
 
-    def test_issues_clossing(self):
-        initial_number_of_issues = len(git.open_issues())
-        self.test_issues_increament()
-        current_open_issues = len(git.open_issues())
-        assert initial_number_of_issues == current_open_issues
+    # changes the state of an issue to close
+    def close_issue(self, number: int):
+        endpoint = self.endpoint + f'/{number}'
+        body = {'state': 'closed', 'state_reason': 'not_planned'}
+        response = requests.patch(endpoint,  json.dumps(body), headers=self.headers)
+        assert response.status_code == 200
 
+    # validates issue has been correctly closed
     def test_issue_eliminated(self, number: int):
         issues = self.open_issues()
-        cond = True
-        for issue in issues:
-            if issue['number'] == number:
-                cond = False
-        assert cond
-
+        assert not any(issue['number'] == number for issue in issues)
 
 
 if __name__ == '__main__':
@@ -77,7 +72,7 @@ if __name__ == '__main__':
     git.test_issues_increament(initial_number_of_issues, initial_issues[0])
 
     # 6
-    git.update_issue_state(first_new_issue_id)
+    git.close_issue(first_new_issue_id)
 
     # 7
     git.test_issue_eliminated(first_new_issue_id)
