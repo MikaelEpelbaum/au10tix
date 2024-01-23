dc8d9fb89fd352794a607079838615729ca816c9
MikaelEpelbaum <mikaelepelbaum@gmail.com>
2024-01-23 12:13:09 +0200

    two first get issues exercise

--- main.py
+++ main.py
@@ -3,16 +3,24 @@ import json
 
 conf = json.load(open('configuration.json'))
 
-# endpoint = 'https://github.com/{}/{}/issues'.format(conf['rep_owner'], conf['rep_name'])
-endpoint = 'https://api.github.com/repos/topq-practice/api-practice/issues'
-
-
 headers = {
     'Accept': 'application/vnd.github+json',
     'Authorization': "Bearer " + conf['token'],
     'X-GitHub-Api-Version': '2022-11-28'
 }
+endpoint = 'https://api.github.com/repos/{}/{}/issues'.format(conf['rep_owner'], conf['rep_name'])
 
-x = requests.request('GET', endpoint, headers=headers).json()
+def num_of_open_issues():
+    response = requests.get(endpoint, headers=headers).json()
+    open_issues = [issue for issue in response if issue['state'] == 'open']
+    print(len(open_issues))
 
-print(x)
+def num_of_labeled_issues(targ_label: str):
+    response = requests.get(endpoint, headers=headers).json()
+    labled_issues = [issue for issue in response if any(label.get('name') == targ_label for label in issue.get('labels', []))]
+    print(len(labled_issues))
+
+
+if __name__ == '__main__':
+    num_of_open_issues()
+    num_of_labeled_issues('practice1')
